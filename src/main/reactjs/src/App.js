d5fb6baf6374043ad2d1bf9efd98d45a16cb2ed6
helloa1109 <helloa1109@naver.com>
2023-06-22 12:39:15 +0900

    gradle수정

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-import homin from './homin.gif';
-import homin1 from './hominpee.gif';
 function App() {
   return (
     <div className="App">
-      <img src={homin} width={800} className='App-logo'/>
-      <img src={homin1} width={600} className='App-logo'/>
-      <img src={homin} width={800} className='App-logo'/>
-      <img src={homin1} width={600} className='App-logo'/>
-      <img src={homin} width={800} className='App-logo'/>
-      <img src={homin1} width={600} className='App-logo'/>
-      <br/><br/>
-      {/* <h1 style={{fontSize:'30px'}} className='App-text'>도커 쉽다 ㅋ </h1> */}
-
+      <h1>dd</h1>
     </div>
   );
 }
